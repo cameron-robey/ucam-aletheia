fb253bbc4fec375488f5acff74a0c3b462782588
Cameron Robey <cameron@cameronrobey.co.uk>
2020-09-20 15:42:16 +0100

    Environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import * as dotenv from 'dotenv';
+
 const app = express();
+dotenv.config();
 
 // Controllers
 import { getCrsid } from './controllers/crsid';
@@ -8,6 +11,7 @@ import { getCrsid } from './controllers/crsid';
 app.get('/crsid/:crsid', getCrsid);
 
 // Set up server
-app.listen(7000, () => {
-  console.log('Server started on port 7000');
+const port: Number = parseInt(process.env.PORT || '7000');
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });

270e780ad03adcbb38500ef6c235748f54170879
Cameron Robey <cameron@cameronrobey.co.uk>
2020-09-20 17:01:13 +0100

    API Key auth

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import { handleAuth } from './auth';
 
 const app = express();
 dotenv.config();
@@ -7,8 +8,15 @@ dotenv.config();
 // Controllers
 import { getCrsid } from './controllers/crsid';
 
+// CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, API-Key')
+  next()
+});
+
 // Routes
-app.get('/crsid/:crsid', getCrsid);
+app.get('/crsid/:crsid', handleAuth, getCrsid);
 
 // Set up server
 const port: Number = parseInt(process.env.PORT || '7000');
